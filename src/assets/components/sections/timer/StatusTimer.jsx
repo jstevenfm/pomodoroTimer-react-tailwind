a031ead97fec1d710f22c23331494232d7886cd5
Jeisson Fonseca <jstevenfm@gmail.com>
2025-03-20 21:18:21 -0500

    refactor: clean up StatusTimer component and improve code formatting

--- src/assets/components/sections/timer/StatusTimer.jsx
+++ src/assets/components/sections/timer/StatusTimer.jsx
@@ -1,35 +1,40 @@
-import RadioButton from "./RadioButton"
+import RadioButton from "./RadioButton";
 
 const typeButton = [
   {
     id: "pomodoro",
     text: "Pomodoro",
-    initialChecked: true
+    initialChecked: true,
   },
   {
     id: "shortBreak",
     text: "Short Break",
-    initialChecked: false
+    initialChecked: false,
   },
   {
     id: "longBreak",
     text: "Long Break",
-    initialChecked: false
-  }
-]
+    initialChecked: false,
+  },
+];
 
 const StatusTimer = () => {
   return (
     <>
       <fieldset className="bg-yellow-100 w-full grid grid-cols-3 rounded-full shadow-md">
         {/* <RadioButton htmlFor="pomodoro" id="pomodoro" text="Pomodoro" checked/> */}
-        {typeButton.map( button => (
-          <RadioButton key={button.id} htmlFor={button.id} id={button.id} text={button.text} checked={button.initialChecked}
+        {typeButton.map((button) => (
+          <RadioButton
+            key={button.id}
+            htmlFor={button.id}
+            id={button.id}
+            text={button.text}
+            checked={button.initialChecked}
           />
         ))}
       </fieldset>
     </>
-  )
-}
+  );
+};
 
-export default StatusTimer
+export default StatusTimer;
